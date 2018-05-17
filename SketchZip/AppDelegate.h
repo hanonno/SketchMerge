02c165e655fd982dca607c911c982903a2a23428
Hanno ten Hoor <hanonno@gmail.com>
2018-05-17 14:24:30 +0200

    Initial Commit

---
+++ SketchZip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SketchZip
+//
+//  Created by Hanno on 17/05/2018.
+//  Copyright © 2018 Motion Pixel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SketchZip/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SketchZip
+//
+//  Created by Hanno on 17/05/2018.
+//  Copyright © 2018 Motion Pixel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
