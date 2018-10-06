e5792d6e406815c5a9467769969dd74808598f49
Hanno ten Hoor <hanonno@gmail.com>
2018-10-06 17:17:56 +0200

    Building a new browser based on realm

---
+++ SketchZip/Indexer/SketchItemBrowser.h
@@ -0,0 +1,14 @@
+//
+//  SketchItemBrowserController.h
+//  SketchZip
+//
+//  Created by Hanno ten Hoor on 06/10/2018.
+//  Copyright © 2018 Motion Pixel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SketchItemBrowser : NSViewController
+
+@end
---
+++ SketchZip/Indexer/SketchItemBrowser.m
@@ -0,0 +1,22 @@
+//
+//  SketchItemBrowserController.m
+//  SketchZip
+//
+//  Created by Hanno ten Hoor on 06/10/2018.
+//  Copyright © 2018 Motion Pixel. All rights reserved.
+//
+
+#import "SketchItemBrowser.h"
+
+@interface SketchItemBrowser ()
+
+@end
+
+@implementation SketchItemBrowser
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
